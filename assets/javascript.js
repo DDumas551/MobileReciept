29fb9f5e651ad4b3f11a8c57c0135bbc7ca87e43
DDumas551 <david.j.dumas@gmail.com>
2019-03-08 07:41:25 -0800

    Version 1.0

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,33 @@
-let jobStart;
-let hourlyRate;
-let truckRate;
-let supplies;
+let employees;
+let date;
+let hours = 0;
+let hourlyRate = 0;
+let hoursTotal = 0;
+let truckRate = 0;
+let supplies = 0;
+let prepaid = 0;
+let subtotal = 0;
 
 
 $("#submitBtn").on("click", function() {
-    jobStart = $("#jobStart").val();
+    employees = $("#names").val();
+    date = $("#todaysDate").val();
+    hours = $("#hours").val();
     hourlyRate = $("#hourlyRate").val();
+    hoursTotal = hours * hourlyRate;
     truckRate = $("#truckRate").val();
-    supplies = $("#supplies").val();  
-    console.log(jobStart);
+    supplies = $("#supplies").val();
+    prepaid = $("#prepaid").val();
+    subtotal = (parseInt(hoursTotal) + parseInt(truckRate) + parseInt(supplies)) - parseInt(prepaid);
+
+    $(".eePTag").append("Workers: " + employees);
+    $(".datePTag").append("Today's Date: " + date);
+    $(".hoursPTag").append("Hours worked: " + hours);
+    $(".ratePTag").append("Hourly Rate: $" + hourlyRate);
+    $(".hoursTotalPTag").append("Labor Total: $" + hoursTotal);
+    $(".truckPtag").append("Truck Rate: $" + truckRate);
+    $(".suppliesPtag").append("Supplies Cost: $" + supplies);
+
+    $(".subtotal").append("Job Subtotal: $" + subtotal);
+    console.log(hoursTotal);
 });
