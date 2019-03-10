bb4ccb2b5ed3b8bf512b4ad31b148e12f092b705
DDumas551 <david.j.dumas@gmail.com>
2019-03-09 19:34:10 -0800

    Beta test ready

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,34 +1,46 @@
 let employees;
 let date;
-let hours = 0;
+let firstTwoHours = 0;
+let additionalHours = 0;
 let hourlyRate = 0;
 let hoursTotal = 0;
 let truckRate = 0;
 let supplies = 0;
 let prepaid = 0;
+let sub = 0;
 let subtotal = 0;
+// let subSub = 0;
 
 
 $("#submitBtn").on("click", function() {
     employees = $("#names").val();
     date = $("#todaysDate").val();
-    hours = $("#hours").val();
+    firstTwoHours = $("#firstTwoHours").val();
+    additionalHours = $("#additionalHours").val();
     hourlyRate = $("#hourlyRate").val();
-    hoursTotal = hours * hourlyRate;
+    if(additionalHours > 0) {
+        hoursTotal = parseInt(additionalHours * hourlyRate);
+    }else{
+        hoursTotal = 0;
+    }
     truckRate = $("#truckRate").val();
     supplies = $("#supplies").val();
     prepaid = $("#prepaid").val();
-    // subtotal = parseInt(hoursTotal) + parseInt(truckRate) + parseInt(supplies) + parseInt(-prepaid);
+    sub = parseInt(hoursTotal) + parseInt(truckRate) + parseInt(supplies);
+    subtotal =  parseInt(sub - prepaid);
+    console.log("sub " + sub);
+    console.log("prepaid " + prepaid);
+    console.log("subtotal" + subtotal);
 
     $(".eePTag").append("Workers: " + employees);
     $(".datePTag").append("Today's Date: " + date);
-    $(".hoursPTag").append("Hours worked: " + hours);
+    $(".firstTwoHoursPTag").append("First Two Hours: $" + firstTwoHours);
+    $(".hoursPTag").append("Aditional Hours: " + additionalHours);
     $(".ratePTag").append("Hourly Rate: $" + hourlyRate);
     $(".hoursTotalPTag").append("Labor Total: $" + hoursTotal);
     $(".truckPtag").append("Truck Rate: $" + truckRate);
     $(".suppliesPtag").append("Supplies Cost: $" + supplies);
+    $(".prepaidPtag").append("Prepaid Amt: $" + prepaid);
+    $(".subtotal").append("Total: $" + (parseInt(subtotal) + parseInt(firstTwoHours)));
 
-    $(".subtotal").append("Job Subtotal: $" + 5);
-    console.log(hoursTotal);
-    console.log(subtotal);
 });
